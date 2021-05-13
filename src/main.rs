5b09815dc6c349131588fe6ed47c0867c0aad757
Christopher N. Hesse <raymanfx@gmail.com>
2021-05-13 13:12:52 +0200

    Finish chapter 3
    Signed-off-by: Christopher N. Hesse <raymanfx@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,16 @@ use ppm::Image;
 mod vec3;
 use vec3::Vec3;
 
-/// RGB color with each channel ranging from 0 to 255
-type Color = Vec3<u8>;
+/// RGB color with each channel ranging from 0.0 to 1.0
+type Color = Vec3<f64>;
+
+/// Transform color values from [0.0, 1.0] to [0, 255].
+fn write_color(color: &Color) {
+    let r = (255.999 * color.x()) as u8;
+    let g = (255.999 * color.y()) as u8;
+    let b = (255.999 * color.z()) as u8;
+    println!("{} {} {}", r, g, b);
+}
 
 fn main() -> io::Result<()> {
     // Image settings
@@ -16,7 +24,7 @@ fn main() -> io::Result<()> {
     const IMAGE_HEIGHT: usize = 256;
 
     // create the image buffer
-    let mut img = Image::new(IMAGE_WIDTH, IMAGE_HEIGHT, Color::new(0, 0, 0));
+    let mut img = Image::new(IMAGE_WIDTH, IMAGE_HEIGHT, Color::new(0.0, 0.0, 0.0));
 
     // fill image with test data
     for j in (0..img.height()).rev() {
@@ -27,11 +35,7 @@ fn main() -> io::Result<()> {
             let r = (i as f64) / ((img.width() - 1) as f64);
             let g = (j as f64) / ((img.height() - 1) as f64);
             let b = 0.25;
-
-            let ir = (255.999 * r) as u8;
-            let ig = (255.999 * g) as u8;
-            let ib = (255.999 * b) as u8;
-            img[i][j] = Color::new(ir, ig, ib);
+            img[i][j] = Color::new(r, g, b);
         }
     }
     eprintln!("\n>> Render done");
@@ -44,7 +48,7 @@ fn main() -> io::Result<()> {
     for j in 0..img.height() {
         for i in 0..img.width() {
             let pix = img[j][i];
-            println!("{} {} {}", pix[0], pix[1], pix[2]);
+            write_color(&pix);
         }
     }
 
